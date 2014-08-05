13df697aa9d4ec995cbe7aecc9188816be777e67
chous <chous@acm-sl.org>
2014-08-05 07:27:50 +0200

    Minor change

--- src/test/java/org/acmsl/commons/utils/EnglishGrammarUtilsTest.java
+++ src/test/java/org/acmsl/commons/utils/EnglishGrammarUtilsTest.java
@@ -1,199 +1,65 @@
+//;-*- mode: java -*-
 /*
-                      Project tests
+                        ACM-SL Commons
 
-Copyright (C) 2003  Jose San Leandro Armend?riz
-jsanleandro@yahoo.es
-chousz@yahoo.com
+    Copyright (C) 2002-today  Jose San Leandro Armendariz
+                              chous@acm-sl.org
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
 
-You should have received a copy of the GNU General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-Thanks to ACM S.L. for distributing this library under the GPL license.
-Contact info: jsr000@terra.es
-Postal Address: c/Playa de Lagoa, 1
-Urb. Valdecaba?as
-Boadilla del monte
-28660 Madrid
-Spain
+    Thanks to ACM S.L. for distributing this library under the LGPL license.
+    Contact info: jose.sanleandro@acm-sl.com
 
-******************************************************************************
-*
-* Filename: $RCSfile$
-*
-* Author: Jose San Leandro Armend?riz
-*
-* Description: Executes all tests defined for package
-*              org.acmsl.commons.utils.
-*
-* Last modified by: $Author: chous $ at $Date: 2006-04-02 12:15:23 +0200 (Sun, 02 Apr 2006) $
-*
-* File version: $Revision: 548 $
-*
-* Project version: $Name$
-*
-* $Id: EnglishGrammarUtilsTest.java 548 2006-04-02 10:15:23Z chous $
-*/
+ ******************************************************************************
+ *
+ * Filename: EnglishGrammarUtilsTest.java
+ *
+ * Author: Jose San Leandro Armendariz
+ *
+ * Description: Tests for EnglishGrammarUtils.
+ */
 package org.acmsl.commons.utils;
 
 /*
-* Importing project classes.
-*/
-// JUnitDoclet begin import
-import junit.framework.Assert;
-import org.acmsl.commons.utils.EnglishGrammarUtils;
-// JUnitDoclet end import
+ * Importing Jetbrains annotations.
+ */
+import org.jetbrains.annotations.NotNull;
 
 /*
-* Importing JUnit classes.
-*/
-import junit.framework.TestCase;
-
-/*
-This file is part of  JUnitDoclet, a project to generate basic
-test cases  from source code and  helping to keep them in sync
-during refactoring.
-
-Copyright (C) 2002  ObjectFab GmbH  (http://www.objectfab.de/)
-
-This library is  free software; you can redistribute it and/or
-modify  it under the  terms of  the  GNU Lesser General Public
-License as published  by the  Free Software Foundation; either
-version 2.1  of the  License, or  (at your option)  any  later
-version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or  FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You  should  have  received a  copy of the  GNU Lesser General
-Public License along with this  library; if not, write  to the
-Free  Software  Foundation, Inc.,  59 Temple Place,  Suite 330,
-Boston, MA  02111-1307  USA
-*/
-
+ * Importing JUnit classes.
+ */
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
- * Tests EnglishGrammarUtilsTest class.
- * @version $Revision: 548 $
+ * Tests for EnglishGrammarUtils.
  * @see org.acmsl.commons.utils.EnglishGrammarUtils
  */
+@RunWith(JUnit4.class)
 public class EnglishGrammarUtilsTest
-// JUnitDoclet begin extends_implements
-    extends TestCase
-// JUnitDoclet end extends_implements
 {
-    // JUnitDoclet begin class
-    org.acmsl.commons.utils.EnglishGrammarUtils englishgrammarutils = null;
-    // JUnitDoclet end class
-
-    /**
-     * Creates a EnglishGrammarUtilsTest with given name.
-     * @param name such name.
-     */
-    public EnglishGrammarUtilsTest(String name)
-    {
-        // JUnitDoclet begin method EnglishGrammarUtilsTest
-        super(name);
-        // JUnitDoclet end method EnglishGrammarUtilsTest
-    }
-
-    public static void main(String[] args)
-    {
-        // JUnitDoclet begin method testcase.main
-        junit.textui.TestRunner.run(EnglishGrammarUtilsTest.class);
-        // JUnitDoclet end method testcase.main
-    }
-
-    /**
-     * Creates an instance of the tested class.
-     * @return such instance.
-
-     */
-    public org.acmsl.commons.utils.EnglishGrammarUtils createInstance()
-        throws Exception
-    {
-        // JUnitDoclet begin method testcase.createInstance
-        return org.acmsl.commons.utils.EnglishGrammarUtils.getInstance();
-        // JUnitDoclet end method testcase.createInstance
-    }
-
-    /**
-     * Performs any required steps before each test.
-     * @throws Exception if an unexpected situation occurs.
-     */
-    protected void setUp()
-        throws Exception
-    {
-        // JUnitDoclet begin method testcase.setUp
-        super.setUp();
-        englishgrammarutils = createInstance();
-        // JUnitDoclet end method testcase.setUp
-    }
-
-    /**
-     * Performs any required steps after each test.
-     * @throws Exception if an unexpected situation occurs.
-     */
-    protected void tearDown()
-        throws Exception
-    {
-        // JUnitDoclet begin method testcase.tearDown
-        englishgrammarutils = null;
-        super.tearDown();
-        // JUnitDoclet end method testcase.tearDown
-    }
-
-    /**
-     * Tests EnglishGrammarUtilsTest#getInstance()
-     * @throws Exception if an unexpected situation occurs.
-     * @see org.acmsl.commons.utils.EnglishGrammarUtils#getInstance()
-     */
-    public void testGetInstance()
-        throws Exception
-    {
-        // JUnitDoclet begin method getInstance
-        EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
-
-        assertNotNull(t_EnglishGrammarUtils);
-        // JUnitDoclet end method getInstance
-    }
-
-    /**
-     * JUnitDoclet moves marker to this method, if there is not match
-     * for them in the regenerated code and if the marker is not empty.
-     * This way, no test gets lost when regenerating after renaming.
-     * Method testVault is supposed to be empty.
-     * @throws Exception if an unexpected situation occurs.
-     */
-    public void testVault()
-        throws Exception
-    {
-        // JUnitDoclet begin method testcase.testVault
-        // JUnitDoclet end method testcase.testVault
-    }
-
     /**
      * Tests {EnglishGrammarUtils#getRegularSingularForm(String)}.
      * @see EnglishGrammarUtils#getRegularSingularForm(String)
      */
-    @SuppressWarnings("unused")
-    public void testGetRegularSingularForm()
+    @Test
+    public void getRegularSingularForm_works()
     {
-        EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
-
-        Assert.assertNotNull(t_EnglishGrammarUtils);
+        @NotNull final EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
 
         Assert.assertEquals(
             "Invalid singular for cars",
@@ -241,12 +107,10 @@ public class EnglishGrammarUtilsTest
      * Tests {EnglishGrammarUtils#getRegularPluralForm(String)}.
      * @see EnglishGrammarUtils#getRegularPluralForm(String)
      */
-    @SuppressWarnings("unused")
-    public void testGetRegularPluralForm()
+    @Test
+    public void getRegularPluralForm_works()
     {
-        EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
-
-        Assert.assertNotNull(t_EnglishGrammarUtils);
+        @NotNull final EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
 
         Assert.assertEquals(
             "Invalid plural for car",
@@ -286,12 +150,10 @@ public class EnglishGrammarUtilsTest
      * Tests {EnglishGrammarUtils#getSingular(String)}.
      * @see EnglishGrammarUtils#getSingular(String)
      */
-    @SuppressWarnings("unused")
-    public void testGetSingular()
+    @Test
+    public void getSingular_works()
     {
-        EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
-
-        Assert.assertNotNull(t_EnglishGrammarUtils);
+        @NotNull final EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
 
         Assert.assertEquals(
             "Invalid singular for cars",
@@ -339,11 +201,10 @@ public class EnglishGrammarUtilsTest
      * Tests {EnglishGrammarUtils#getPlural(String)}.
      * @see EnglishGrammarUtils#getPlural(String)
      */
-    public void testGetPlural()
+    @Test
+    public void getPlural_works()
     {
-        EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
-
-        Assert.assertNotNull(t_EnglishGrammarUtils);
+        @NotNull final EnglishGrammarUtils t_EnglishGrammarUtils = EnglishGrammarUtils.getInstance();
 
         Assert.assertEquals(
             "Invalid plural for car",
